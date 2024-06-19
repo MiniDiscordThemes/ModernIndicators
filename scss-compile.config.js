1e70fb7c3b2a27fd912dfe7a6bb2707ec125e44f
Saltssaumure <themes@saltssaumure.net>
2024-06-19 20:09:00 +0100

    Add other client mod testing capability

---
+++ scss-compile.config.js
@@ -0,0 +1,9 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        name: "ModernIndicators" /* The official name of your theme. */,
+        scss: "modind" /* The name of your base scss file. */,
+        repo: "ModernIndicators" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
+    }
+};
